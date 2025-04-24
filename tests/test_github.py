98c1440715d107bfe3e8a8903c3e85da837f2640
Andrey Okatev <okatev87@yandex.ru>
2025-04-24 12:50:55 +0300

    fix

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,3 +1,5 @@
+import os
+
 import allure
 from allure_commons.types import Severity
 from selene import have
@@ -9,40 +11,43 @@ from pages.issues_page import Issue
 from tests.confest import browser_setup
 
 
-def test_github_by_selene(browser_setup):
-    browser.open('https://github.com')
-    s('.search-input').click()
-    s('#query-builder-test').send_keys('87stels87/qa_guru_hw1').press_enter()
-    s(by.link_text('87stels87/qa_guru_hw1')).click()
-    s('#issues-tab').click()
-    s(by.css(".blankslate-heading")).should(have.exact_text('No results'))
+# def test_github_by_selene(browser_setup):
+#     browser.open('https://github.com')
+#     s('.search-input').click()
+#     s('#query-builder-test').send_keys('87stels87/qa_guru_hw1').press_enter()
+#     s(by.link_text('87stels87/qa_guru_hw1')).click()
+#     s('#issues-tab').click()
+#     s(by.css(".blankslate-heading")).should(have.exact_text('No results'))
+#
+#
+# def test_github_with_allure_step(browser_setup):
+#     with allure.step("Открываем главную страницу гитхаб"):
+#         browser.open('https://github.com')
+#     with allure.step("Ищем репозиторий"):
+#         s('.search-input').click()
+#         s('#query-builder-test').send_keys('87stels87/qa_guru_hw1').press_enter()
+#     with allure.step("Переходим по найденному репозиторию"):
+#         s(by.link_text('87stels87/qa_guru_hw1')).click()
+#     with allure.step("Переходим по вкладку issues"):
+#         s('#issues-tab').click()
+#     with allure.step("Проверяем что открытых issues нет"):
+#         s(by.css(".blankslate-heading")).should(have.exact_text('No results'))
+#
+#
+# @allure.feature("Проверка отображения issues в репозитории")
+# @allure.epic("Гитхаб")
+# @allure.tag("smoke")
+# @allure.severity(Severity.NORMAL)
+# @allure.link("https://github.com", name="issue text")
+# @allure.step("Поиск issues по тексту элемента")
+# @allure.label("owner", "Okatev")
+# def test_github_with_decorator(browser_setup):
+#     issue = Issue()
+#     issue.open_main_page()
+#     issue.search_repo()
+#     issue.go_to_repo()
+#     issue.go_to_issues()
+#     issue.check_issues()
 
-
-def test_github_with_allure_step(browser_setup):
-    with allure.step("Открываем главную страницу гитхаб"):
-        browser.open('https://github.com')
-    with allure.step("Ищем репозиторий"):
-        s('.search-input').click()
-        s('#query-builder-test').send_keys('87stels87/qa_guru_hw1').press_enter()
-    with allure.step("Переходим по найденному репозиторию"):
-        s(by.link_text('87stels87/qa_guru_hw1')).click()
-    with allure.step("Переходим по вкладку issues"):
-        s('#issues-tab').click()
-    with allure.step("Проверяем что открытых issues нет"):
-        s(by.css(".blankslate-heading")).should(have.exact_text('No results'))
-
-
-@allure.feature("Проверка отображения issues в репозитории")
-@allure.epic("Гитхаб")
-@allure.tag("smoke")
-@allure.severity(Severity.NORMAL)
-@allure.link("https://github.com", name="issue text")
-@allure.step("Поиск issues по тексту элемента")
-@allure.label("owner", "Okatev")
-def test_github_with_decorator(browser_setup):
-    issue = Issue()
-    issue.open_main_page()
-    issue.search_repo()
-    issue.go_to_repo()
-    issue.go_to_issues()
-    issue.check_issues()
+def test_complete_form(browser_setup):
+    browser.open('https://ya.ru')
