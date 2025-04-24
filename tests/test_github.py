c8c72d4125f5aadacd816fb51c9878d0b3f782f9
Andrey Okatev <okatev87@yandex.ru>
2025-04-24 13:11:46 +0300

    fix

--- tests/test_github.py
+++ tests/test_github.py
@@ -49,5 +49,40 @@ from tests.confest import browser_setup
 #     issue.go_to_issues()
 #     issue.check_issues()
 
-def test_complete_form(browser_setup):
-    browser.open('https://ya.ru')
+def test_filling_all_fields(browser_setup):
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.driver.maximize_window()
+    browser.element('#firstName').click().type("Иван")
+    browser.element('#lastName').click().type("Иванов")
+    browser.element('#userEmail').click().type("ivanov@ya.ru")
+    browser.element('[value=Female]').double_click()
+    browser.element('[id="userNumber"]').click().type("1234567890")
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('option[value="2"]').click()
+    browser.element('option[value="2000"]').click()
+    browser.element('[aria-label = "Choose Thursday, March 2nd, 2000"]').click()
+    browser.element('#subjectsInput').double_click().set("accounting")
+    browser.element('#react-select-2-option-0').with_(timeout=browser.config.timeout * 2).double_click()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('picture.jpeg'))
+    browser.element('[placeholder="Current Address"]').click().type("Москва")
+    browser.element('#state').click()
+    browser.element('#react-select-3-option-0').click()
+    browser.element('#city').click()
+    browser.element('#react-select-4-option-0').click()
+    browser.element('#submit').click()
+    browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
+    browser.element('table').all('tr').should(have.exact_texts(
+        'Label Values',
+        'Student Name Иван Иванов',
+        'Student Email ivanov@ya.ru',
+        'Gender Female',
+        'Mobile 1234567890',
+        'Date of Birth 02 March,2000',
+        'Subjects Accounting',
+        'Hobbies Reading',
+        'Picture picture.jpeg',
+        'Address Москва',
+        'State and City NCR Delhi')
+    )
