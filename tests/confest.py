63dade07cb097a496ae91bccca1f868f32c14cb5
Andrey Okatev <okatev87@yandex.ru>
2025-04-26 17:08:19 +0300

    fix

--- tests/confest.py
+++ tests/confest.py
@@ -1,10 +1,35 @@
 import pytest
 
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
-@pytest.fixture(scope='session')
-def browser_setup():
-    browser.driver.maximize_window()
-    yield
+@pytest.fixture(scope='function')
+def browser_setup(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
--- tests/test_github.py
+++ tests/test_github.py
@@ -1,21 +1,10 @@
 import allure
-from allure_commons.types import Severity
 from selene import have
 from selene.support import by
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
-from pages.issues_page import Issue
 from tests.confest import browser_setup
-
-
-def test_github_by_selene(browser_setup):
-    browser.open('https://github.com')
-    s('.search-input').click()
-    s('#query-builder-test').send_keys('87stels87/qa_guru_hw1').press_enter()
-    s(by.link_text('87stels87/qa_guru_hw1')).click()
-    s('#issues-tab').click()
-    s(by.css(".blankslate-heading")).should(have.exact_text('No results'))
 
 
 def test_github_with_allure_step(browser_setup):
@@ -30,19 +19,3 @@ def test_github_with_allure_step(browser_setup):
         s('#issues-tab').click()
     with allure.step("Проверяем что открытых issues нет"):
         s(by.css(".blankslate-heading")).should(have.exact_text('No results'))
-
-
-@allure.feature("Проверка отображения issues в репозитории")
-@allure.epic("Гитхаб")
-@allure.tag("smoke")
-@allure.severity(Severity.NORMAL)
-@allure.link("https://github.com", name="issue text")
-@allure.step("Поиск issues по тексту элемента")
-@allure.label("owner", "Okatev")
-def test_github_with_decorator(browser_setup):
-    issue = Issue()
-    issue.open_main_page()
-    issue.search_repo()
-    issue.go_to_repo()
-    issue.go_to_issues()
-    issue.check_issues()
